9951f8be160b6c925bcdd40ff3fcab55d4ae5b95
DpkRn <dwizardtechno@gmail.com>
2024-10-15 22:30:39 +0530

    got some problem fixing

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -9,7 +9,7 @@ const verifyToken = async (req, res, next) => {
     if (token==''||!token) {
       return res
         .status(401)
-        
+        .json({ success: false });
     }
     console.log('verifing')
     const auth = await jwt.verify(token, process.env.JWT_KEY);
