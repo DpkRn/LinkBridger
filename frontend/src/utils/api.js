dd3594c676eaa19155f60973ca4ccc302a6391c7
DpkRn <dwizardtechno@gmail.com>
2024-10-15 13:08:01 +0530

    link added

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: 'http://localhost:8080', // Replace with your API base URL
+  baseURL: 'https://linkb-one.vercel.app', // Replace with your API base URL
 });
 export default api;
