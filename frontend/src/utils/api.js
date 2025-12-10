fa5ffe38e2c40d4bcf568511833b5423c8395dce
DpkRn <deepak@skor.co>
2025-12-11 02:27:42 +0530

    adding docker file to backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: 'https://linkb-one.vercel.app', // Replace with your API base URL
+  // baseURL: 'https://linkb-one.vercel.app', // Replace with your API base URL
   // baseURL:'https://clickly.cv'
-  // baseURL: 'http://localhost:8080', // Replace with your API base URL
+  baseURL: 'http://localhost:8080', // Replace with your API base URL
 
 });
 export default api;
