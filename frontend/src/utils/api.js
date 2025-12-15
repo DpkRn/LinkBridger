73cf3a0e52b707618ba2de1b4044c4cb34834425
DpkRn <deepak@skor.co>
2025-12-16 04:40:05 +0530

    Added about and some enhance is there

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,8 +2,11 @@ import axios from 'axios';
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: 'http://localhost:8080', // Replace with your API base URL
-  // baseURL: import.meta.env.VITE_API_URL || 'https://clickly.cv',
-  // withCredentials: true,
+
+  // baseURL: 'http://localhost:8080', // Replace with your API base URL
+  // baseURL: 'https://linkb-one.vercel.app',
+
+  baseURL: import.meta.env.VITE_API_URL || 'https://clickly.cv',
+  withCredentials: true,
 });
 export default api;
