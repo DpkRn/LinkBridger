e34d26e7c511830431d3001d0140c8f216914609
DpkRn <deepak@skor.co>
2025-12-20 13:35:24 +0530

    increased dark intensity

--- frontend/src/components/DashBoard.jsx
+++ frontend/src/components/DashBoard.jsx
@@ -9,7 +9,11 @@ const DashBoard = () => {
  
 
   return (
+<<<<<<< Updated upstream
     <div className="flex flex-col bg-gradient-to-br from-slate-50 via-blue-50 to-purple-50 dark:from-gray-950 dark:via-purple-950 dark:to-gray-950 transition-colors duration-300">
+=======
+    <div className="flex flex-col dark:from-slate-900 dark:via-purple-900 dark:to-slate-900 dark:bg-gradient-to-br transition-colors duration-300">
+>>>>>>> Stashed changes
       
       <Content/>
       <CreateBridge/>
